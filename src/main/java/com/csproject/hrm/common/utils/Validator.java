0fc02d4c6296ccaa393e1f5301c91bfebff07b5f
hungproh99 <hihihd35@gmail.com>
2022-04-19 22:38:20 +0700

    add

---
+++ src/main/java/com/csproject/hrm/common/utils/Validator.java
@@ -0,0 +1,10 @@
+package com.csproject.hrm.common.utils;
+
+public class Validator {
+	
+	public static boolean isPositiveNumber() {
+		return false;
+		
+	}
+	
+}
---
+++ src/main/java/com/csproject/hrm/controllers/DemoController.java
@@ -0,0 +1,23 @@
+package com.csproject.hrm.controllers;
+
+import com.csproject.hrm.entities.Demo;
+import com.csproject.hrm.services.impl.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "demo")
+public class DemoController {
+	
+	private final DemoService demoService;
+	
+	@Autowired
+	public DemoController(DemoService demoService) {this.demoService = demoService;}
+	
+	
+	@GetMapping(path = "/list", params = "id")
+	public Demo getDemoById(@RequestParam(name = "id") Long id) {
+		return demoService.getDemoByID(id);
+	}
+	
+}
