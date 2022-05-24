0287454332498714fd0b2d0188d0b9d9af8af712
hungproh99 <hihihd35@gmail.com>
2022-05-25 01:40:29 +0700

    update

--- src/main/java/com/csproject/hrm/jwt/JwtUtils.java
+++ src/main/java/com/csproject/hrm/jwt/JwtUtils.java
@@ -1,9 +1,9 @@
 package com.csproject.hrm.jwt;
 
 import com.csproject.hrm.services.impl.UserDetailsImpl;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -15,42 +15,38 @@ import java.util.HashMap;
 @Component
 public class JwtUtils {
     private static final Logger Logger = LoggerFactory.getLogger(JwtUtils.class);
-
+    
     @Value("${bezkoder.app.jwtSecret}")
     private String jwtSecret;
-
+    
     @Value("${bezkoder.app.jwtExpirationMs}")
     private long jwtExpirationMs;
-
+    
     public String generateJwtToken(Authentication authentication) {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
         HashMap<String, Object> map = new HashMap<>();
         map.put("User Data", userPrincipal);
-        return Jwts.builder()
-                .setSubject(userPrincipal.getId())
-                .addClaims(map)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
+        return Jwts.builder().setSubject(userPrincipal.getId()).addClaims(map).setIssuedAt(new Date()).setExpiration(
+                           new Date(new Date().getTime() + jwtExpirationMs)).signWith(SignatureAlgorithm.HS512, jwtSecret)
+                   .compact();
     }
-
+    
     public String getIdFromJwtToken(String token) {
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token).getBody().getSubject();
     }
-
+    
     public boolean validateJwtToken(String authToken) {
-        try {
+        try{
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(authToken);
-        } catch (SignatureException e) {
+        }catch(SignatureException e){
             Logger.error("Invalid JWT signature: {}", e.getMessage());
-        } catch (MalformedJwtException e) {
+        }catch(MalformedJwtException e){
             Logger.error("Invalid JWT token: {}", e.getMessage());
-        } catch (ExpiredJwtException e) {
+        }catch(ExpiredJwtException e){
             Logger.error("JWT token is expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
+        }catch(UnsupportedJwtException e){
             Logger.error("JWT token is unsupported: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
+        }catch(IllegalArgumentException e){
             Logger.error("JWT claims string is empty: {}", e.getMessage());
         }
         return false;
--- src/main/java/com/csproject/hrm/jwt/config/AuthEntryPointJwt.java
+++ src/main/java/com/csproject/hrm/jwt/config/AuthEntryPointJwt.java
@@ -1,25 +1,24 @@
 package com.csproject.hrm.jwt.config;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
-
-    private static final Logger Logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
-
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-            throws IOException, ServletException {
-        Logger.error("Unauthorized error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Error: Unauthorized");
-    }
+	
+	private static final Logger Logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+	
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+	                     AuthenticationException authException) throws IOException {
+		Logger.error("Unauthorized error: {}", authException.getMessage());
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+	}
 }
