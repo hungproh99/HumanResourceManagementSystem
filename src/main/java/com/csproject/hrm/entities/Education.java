57c8cc11cff43840fffea7f1becc28d3c4f7535d
hungproh99 <hihihd35@gmail.com>
2022-05-29 18:09:15 +0700

    update

--- src/main/java/com/csproject/hrm/entities/Education.java
+++ src/main/java/com/csproject/hrm/entities/Education.java
@@ -2,10 +2,36 @@ package com.csproject.hrm.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "education")
 public class Education {
-	private Long educationID;
+	@Id
+	@Column(name = "education_id")
+	private Long id;
+	
+	@Column(name = "name_school")
+	private String nameSchool;
+	
+	@Column(name = "start_date")
+	private LocalDate startDate;
+	
+	@Column(name = "end_date")
+	private LocalDate endDate;
+	
+	@Column(name = "certificate")
+	private String certificate;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "employee_id")
+	private Employee employee;
 }
