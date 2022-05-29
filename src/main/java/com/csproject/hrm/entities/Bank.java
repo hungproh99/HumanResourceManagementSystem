0a23b46d22f8d0f6a0c70029e6580981896f48b0
hungproh99 <hihihd35@gmail.com>
2022-05-29 18:09:15 +0700

    update

--- src/main/java/com/csproject/hrm/entities/Bank.java
+++ src/main/java/com/csproject/hrm/entities/Bank.java
@@ -2,10 +2,31 @@ package com.csproject.hrm.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "bank")
 public class Bank {
-	private Long bankID;
+	@Id
+	@Column(name = "bank_id")
+	private Long id;
+	
+	@Column(name = "name_bank")
+	private String nameBank;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "account_number")
+	private String accountNumber;
+	
+	@Column(name = "account_name")
+	private String accountName;
+	
+	@OneToOne(mappedBy = "bank", fetch = FetchType.LAZY)
+	private Employee employee;
 }
--- src/main/java/com/csproject/hrm/entities/Education.java
+++ src/main/java/com/csproject/hrm/entities/Education.java
@@ -2,6 +2,9 @@ package com.csproject.hrm.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,26 +12,26 @@ import lombok.*;
 @Entity
 @Table(name = "education")
 public class Education {
-    @Id
-    @Column(name = "education_id")
-    private Long id;
-
-    @Column(name = "name_school")
-    private String nameSchool;
-
-    @Column(name = "start_date")
-    private LocalDate startDate;
-
-    @Column(name = "end_date")
-    private LocalDate endDate;
-
-    @Column(name = "certificate")
-    private String certificate;
-
-    @Column(name = "status")
-    private String status;
-
-    @ManyToOne(cascade = CascadeType.ALL, targetEntity = Employee.class)
-    @JoinColumn(name = "employee_id")
-    private Employee employee;
+	@Id
+	@Column(name = "education_id")
+	private Long id;
+	
+	@Column(name = "name_school")
+	private String nameSchool;
+	
+	@Column(name = "start_date")
+	private LocalDate startDate;
+	
+	@Column(name = "end_date")
+	private LocalDate endDate;
+	
+	@Column(name = "certificate")
+	private String certificate;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "employee_id")
+	private Employee employee;
 }
