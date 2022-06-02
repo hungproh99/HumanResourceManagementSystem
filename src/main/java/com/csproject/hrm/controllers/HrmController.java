2a0424f3647df0609cde21a4918dc8b0589dcaef
hungproh99 <hihihd35@gmail.com>
2022-06-02 22:44:38 +0700

    update

--- src/main/java/com/csproject/hrm/controllers/HrmController.java
+++ src/main/java/com/csproject/hrm/controllers/HrmController.java
@@ -1,6 +1,7 @@
 package com.csproject.hrm.controllers;
 
-import com.csproject.hrm.dto.response.HrmResponse;
+import com.csproject.hrm.jooq.Context;
+import com.csproject.hrm.jooq.QueryParam;
 import com.csproject.hrm.services.HumanManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,22 +9,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-import static com.csproject.hrm.common.uri.Uri.*;
+import static com.csproject.hrm.common.uri.Uri.REQUEST_MAPPING;
+import static com.csproject.hrm.common.uri.Uri.URI_GET_ALL_EMPLOYEE;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(REQUEST_MAPPING)
 public class HrmController {
-
-    @Autowired
-    HumanManagementService humanManagementService;
-
-    @PostMapping(URI_GET_ALL_EMPLOYEE)
-    public ResponseEntity<?> getAllEmployee(@RequestParam Map<String, String> allRequestParams) {
-//        List<HrmResponse> hrmResponses = humanManagementService.getListHumanResource(limit, page);
-        Context context = new Context();
-        QueryParam queryParam = context.queryParam(allRequestParams);
-        return ResponseEntity.ok(queryParam.filters + "\n" + queryParam.pagination + "\n" + queryParam.orderByList);
-
-    }
+	
+	@Autowired
+	HumanManagementService humanManagementService;
+	
+	@PostMapping(URI_GET_ALL_EMPLOYEE)
+	public ResponseEntity<?> getAllEmployee(@RequestParam Map<String, String> allRequestParams) {
+		//        List<HrmResponse> hrmResponses = humanManagementService.getListHumanResource(limit, page);
+		Context context = new Context();
+		QueryParam queryParam = context.queryParam(allRequestParams);
+		return ResponseEntity.ok(queryParam.filters + "\n" + queryParam.pagination + "\n" + queryParam.orderByList);
+		
+	}
 }
--- src/main/java/com/csproject/hrm/jwt/config/AuthTokenFilter.java
+++ src/main/java/com/csproject/hrm/jwt/config/AuthTokenFilter.java
@@ -19,39 +19,40 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
-	
-	private static final Logger Logger = LoggerFactory.getLogger(AuthTokenFilter.class);
-	@Autowired
-	private JwtUtils jwtUtils;
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-	                                FilterChain filterChain) throws ServletException, IOException {
-		try{
-			String jwt = parseJwt(request);
-			if(jwt != null && jwtUtils.validateJwtToken(jwt)){
-				String id = jwtUtils.getIdFromJwtToken(jwt);
-				UserDetails userDetails = userDetailsService.loadUserByUsername(id);
-				UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null,
-				                                                                                   userDetails.getAuthorities());
-				auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(auth);
-			}
-		}catch(Exception e){
-			logger.error("Cannot set user authentication: {}", e);
-		}
-		filterChain.doFilter(request, response);
-	}
-	
-	private String parseJwt(HttpServletRequest request) {
-		
-		String headerAuth = request.getHeader("Authorization");
-		
-		if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
-			return headerAuth.substring(7);
-		}
-		return null;
-	}
+
+    private static final Logger Logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String id = jwtUtils.getIdFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(id);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
 }
