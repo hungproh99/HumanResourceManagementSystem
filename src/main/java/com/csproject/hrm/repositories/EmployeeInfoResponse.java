5e6a6f20fb2ce39fcbaf57860762d789bbc99064
hungproh99 <hihihd35@gmail.com>
2022-06-03 18:07:43 +0700

    update

--- src/main/java/com/csproject/hrm/repositories/EmployeeInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package com.csproject.hrm.repositories;
-
-import com.csproject.hrm.entities.Employee;
-
-import java.util.List;
-
-public interface EmployeeInfoResponse {
-	List<Employee> getCustomEmployee();
-}
--- src/main/java/com/csproject/hrm/services/impl/HumanManagementServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.csproject.hrm.services.impl;
-
-import com.csproject.hrm.dto.response.HrmResponse;
-
-import java.util.List;
-
-public interface HumanManagementServiceImpl {
-    List<HrmResponse> getListHumanResource(String limit, String page);
-}
