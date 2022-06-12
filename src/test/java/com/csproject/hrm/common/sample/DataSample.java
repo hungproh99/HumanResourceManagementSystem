05ee08c01ce3c61353664c7be859916c16cc051d
hungproh99 <hihihd35@gmail.com>
2022-06-12 22:48:18 +0700

    update

---
+++ src/test/java/com/csproject/hrm/common/sample/DataSample.java
@@ -0,0 +1,25 @@
+package com.csproject.hrm.common.sample;
+
+import com.csproject.hrm.common.utils.DateUtils;
+import com.csproject.hrm.dto.response.EmployeeDetailResponse;
+
+public class DataSample {
+	public static EmployeeDetailResponse DETAIL_RESPONSE = new EmployeeDetailResponse("Nguyen Thi An",
+	                                                                   "ANT1",
+	                                                                   null,
+	                                                                   null,
+	                                                                   DateUtils.convert("2019-01-02"),
+	                                                                   "Deactivate",
+	                                                                   "url",
+	                                                                   "1234567891",
+	                                                                   null,
+	                                                                   DateUtils.convert("2000-09-01"),
+	                                                                   "ANT11@fpt.edu.vn",
+	                                                                   "Male",
+	                                                                   "Alone",
+	                                                                   "Ha Noi Office",
+	                                                                   null,
+	                                                                   "Back Office",
+	                                                                   "https://haycafe.vn/wp-content/uploads/2022/03/Avatar-hai-doc-600x600.jpg",
+	                                                                   "Part Time");
+}
