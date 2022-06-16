0b612726f013d2948ba387e19edf482ecb7da9f8
ADMIN <ADMIN@DESKTOP-62QRITH>
2022-06-16 11:59:21 +0700

    fix bug email invalid

--- src/main/java/com/csproject/hrm/common/constant/Constants.java
+++ src/main/java/com/csproject/hrm/common/constant/Constants.java
@@ -2,7 +2,7 @@ package com.csproject.hrm.common.constant;
 
 public class Constants {
   public static final String EMAIL_VALIDATION =
-      "^[a-z][a-z0-9_\\.]{5,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$";
+      "^[a-z][a-z0-9_\\.]{3,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$";
   public static final String FROM_EMAIL = "huynq08120@gmail.com";
   public static final String TO_EMAIL = "hihihd37@gmail.com";
   public static final String NOT_EMPTY_EMAIL = "Email can't not empty";
