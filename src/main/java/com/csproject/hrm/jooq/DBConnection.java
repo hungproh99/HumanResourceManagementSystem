b2ec3b68c8afc48edf60c7111ec82224c83df0c9
hungproh99 <hihihd35@gmail.com>
2022-07-26 02:31:17 +0700

    update

--- src/main/java/com/csproject/hrm/jooq/DBConnection.java
+++ src/main/java/com/csproject/hrm/jooq/DBConnection.java
@@ -18,8 +18,6 @@ public class DBConnection {
     try {
       connection =
           DriverManager.getConnection(
-              // "jdbc:mysql://hrm-database.c3lkjkkyx1it.ap-southeast-1.rds.amazonaws.com/human_resource_management",
-              //              "root", "hrmroot01");
               "jdbc:mysql://localhost:3306/human_resource_management", "ndh4899", "hungnd");
     } catch (SQLException e) {
       throw new RuntimeException(e);
