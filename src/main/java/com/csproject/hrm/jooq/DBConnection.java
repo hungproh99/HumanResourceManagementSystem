860bd20c5b96b1793565f48613fbab9d007fefec
ADMIN <ADMIN@DESKTOP-62QRITH>
2022-08-04 20:51:55 +0700

    update db connection

--- src/main/java/com/csproject/hrm/jooq/DBConnection.java
+++ src/main/java/com/csproject/hrm/jooq/DBConnection.java
@@ -3,7 +3,9 @@ package com.csproject.hrm.jooq;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Component
 @Data
@@ -18,13 +20,9 @@ public class DBConnection {
     try {
       connection =
           DriverManager.getConnection(
-              //
-              // "jdbc:mysql://awseb-e-3tmtipzvke-stack-awsebrdsdatabase-eqfxaihqukay.cmdnwkpbg63s.us-west-2.rds.amazonaws.com:3306/human_resource_management?autoReconnect=true",
-              //              "root",
-              //              "123456789");
-              "jdbc:mysql://localhost:3306/human_resource_management?autoReconnect=true",
-              "ndh4899",
-              "hungnd");
+              "jdbc:mysql://awseb-e-3tmtipzvke-stack-awsebrdsdatabase-eqfxaihqukay.cmdnwkpbg63s.us-west-2.rds.amazonaws.com:3306/human_resource_management?autoReconnect=true",
+              "root",
+              "123456789");
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
